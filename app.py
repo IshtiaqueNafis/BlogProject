3eee50e2f828c20d1819118b852160efabda185c
user.email <46496245+Fallofduty@users.noreply.github.com>
2021-03-06 00:03:06 -0500

    added redirection template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -20,16 +20,17 @@ def home():
     return render_template('home.html', posts=posts)
 
 
-# in here post is being passed to post.
-# vatirable will have access to it
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 
 @app.route("/login")
-def register():
+def login():
     form = LoginForm()
     return render_template('login.html', title='login', form=form)
 
