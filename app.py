59e1dfadfdf8e33b58a192eef709772f9248acd3
user.email <46496245+Fallofduty@users.noreply.github.com>
2021-03-05 21:25:28 -0500

    added how to pass values

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+posts = [
+    {
+        'author': 'Nafis ishtiaque',
+        'title': 'Blog post 1',
+        "content": "first post content",
+        'date_posted': "april 20,2018"
+    }
+
+]
 
 
 @app.route('/')  # this is the home
 @app.route('/home')  # this is means its the home
 def home():
-    return render_template('home.html')
+    return render_template('home.html',posts=posts,title='test')
+# in here post is being passed to post.
+#vatirable will have access to it
+
 
 
 @app.route("/about")
