4ec10beffa2fefe3f35d89bce62d3c0c5ebff35b
user.email <46496245+Fallofduty@users.noreply.github.com>
2021-03-05 23:21:16 -0500

    added registeration and login class

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template,url_for
+from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "8198926847042665"
 posts = [
     {
         'author': 'Nafis ishtiaque',
@@ -15,10 +17,21 @@ posts = [
 @app.route('/')  # this is the home
 @app.route('/home')  # this is means its the home
 def home():
-    return render_template('home.html',posts=posts,title='test')
-# in here post is being passed to post.
-#vatirable will have access to it
+    return render_template('home.html', posts=posts)
 
+
+# in here post is being passed to post.
+# vatirable will have access to it
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login")
+def register():
+    form = LoginForm()
+    return render_template('login.html', title='login', form=form)
 
 
 @app.route("/about")
